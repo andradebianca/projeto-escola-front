7d46a7daee85a381497fa1f6f48b65cdbb8edac2
Guilherme Suave <guilherme.suave@el.com.br>
2026-05-21 19:35:53 -0300

    fix: colocando parametro de cod da turma na api

--- routes/nota.routes.js
+++ routes/nota.routes.js
@@ -30,6 +30,7 @@ router.get(
             n.data_aplicacao,
             n.data_criacao,
             n.periodo_nota,
+            n.cod_turma,
 
             a.id_aluno,
             a.nome_completo AS aluno,
